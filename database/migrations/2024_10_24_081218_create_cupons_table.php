6388d0adb43f2b965ee8a6be16a453fa53d07e0a
mlfadilah1 <muhammadlutfi.fadilah96@gmail.com>
2024-11-13 22:50:22 +0700

    perbaruan

--- database/migrations/2024_10_24_081218_create_cupons_table.php
+++ database/migrations/2024_10_24_081218_create_cupons_table.php
@@ -12,10 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('cupons', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('enrollment_id');  // Relasi ke tabel 'enrollments'
-            $table->string('cupon_code',250);  // Menggunakan tipe string untuk kode kupon
-            $table->timestamps();
+            $table->id(); // Primary key (id)
+            $table->string('cupon_code', 250); // Coupon code (varchar)
+            $table->string('description', 255); // Description of the coupon (varchar)
+            $table->enum('discount_type', ['percentage', 'flat']); // Discount type (enum)
+            $table->decimal('discount_value', 8, 2); // Discount value (decimal)
+            $table->date('valid_form'); // Valid from date (date)
+            $table->date('valid_until'); // Valid until date (date)
+            $table->integer('usage_limit'); // Usage limit (integer)
+            $table->integer('total_usage'); // Total usage (integer)
+            $table->timestamps(); // Laravel created_at and updated_at timestamps
         });
         
     }
